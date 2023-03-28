b3e27bea7cd5ea3bb7a2be2d8a40164872801e48
avi v <mr.avi.py@gamil.comgit config --global user.name avi0v1git config --global user.name avi0vgit clearcleatrgit clearall cleargit confi>
2023-03-28 12:15:32 +0530

    adding py file

---
+++ main.py
@@ -0,0 +1,18 @@
+#practise python variable, statments, comments etc
+
+a=b=c=23 
+print(a,b,c)
+print(a+b+c)
+
+d=0b111
+print(d)
+print(type(d))
+
+'''let s be the string name , slicing etc '''
+s="PYTHON"
+print(s[2],s[0:6:2])
+
+''' rough work 
+0-6-2 python-012345 
+2 steps- pxtxox will be asw
+'''
